51021ca4530f4889b3d9df57872a07ffb60c1505
Wen Li <wenli936@gmail.com>
2015-04-10 01:10:34 -0700

    modified css and fixed nav bar display, slightly modify seed file

--- seed.sql
+++ seed.sql
@@ -19,10 +19,10 @@ INSERT INTO permissions (role, user_id) VALUES ('User', 2);
 INSERT INTO permissions (role, user_id) VALUES ('User', 3);
 
 --inserting articles
-INSERT INTO articles (owner_id, subject) VALUES (1, 'this is a subject');
-INSERT INTO articles (owner_id, subject) VALUES (2, 'this is another subject');
-INSERT INTO articles (owner_id, subject) VALUES (3, 'and this is also a subject. wudduya know.');
-INSERT INTO articles (owner_id, subject) VALUES (2, 'and this is also a subject for the fourth time.');
+INSERT INTO articles (owner_id, subject) VALUES (1, 'This is a subject');
+INSERT INTO articles (owner_id, subject) VALUES (2, 'This is another subject');
+INSERT INTO articles (owner_id, subject) VALUES (3, 'And this is also a subject. wudduya know.');
+INSERT INTO articles (owner_id, subject) VALUES (2, 'And this is also a subject for the fourth time.');
 
 --inserting articles/collections
 INSERT INTO articles_collections (article_id, collection_id) VALUES (1,1);
@@ -34,22 +34,26 @@ INSERT INTO articles_collections (article_id, collection_id) VALUES (4,4);
 INSERT INTO categories (article_id, sequence, title) VALUES (1, 1, 'Basics');
 INSERT INTO categories (article_id, sequence, title) VALUES (2, 2, 'Intro');
 INSERT INTO categories (article_id, sequence, title) VALUES (3, 3, 'What is it?');
+INSERT INTO categories (article_id, sequence, title) VALUES (1, 2, 'Advanced');
 
 --inserting sections
-INSERT INTO sections (article_id, title, body, category_id, sequence) values (1, 'There are 10 kinds of people in the world', 'Ones who know binary and ones who do not', 1, 1);
-INSERT INTO sections (article_id, title, body, category_id, sequence) values (2, 'Basics', 'Some basics about the importance of inheritance and stuff', 2, 1);
-INSERT INTO sections (article_id, title, body, category_id, sequence) values (2, 'Examples', 'Some examples about squares being rectangles or something like that',2, 2);
-INSERT INTO sections (article_id, title, body, category_id, sequence) values (2, 'More examples', 'Some more examples about fruits and vegetables',2, 3);
+INSERT INTO sections (article_id, title, body, category_id, sequence) VALUES (1, 'What is UX', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo augue, facilisis eget vulputate facilisis, sagittis et dui. Ut nibh turpis, hendrerit ac enim sit amet, tincidunt interdum libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel porttitor nibh. Suspendisse maximus, neque nec efficitur bibendum, lorem ante imperdiet nisi, nec vehicula sem erat nec orci. Ut et nisl eu nunc dapibus tempor. Ut pulvinar condimentum orci, in accumsan lorem. Proin finibus ipsum iaculis orci fringilla, vel viverra mi lacinia. Aenean nulla sapien, commodo dapibus erat ut, varius accumsan elit. Duis quis metus pulvinar, tincidunt tellus et, ultrices magna. Vivamus et neque ut est suscipit varius nec et tortor. Proin vel massa laoreet, eleifend orci a, finibus mi. Suspendisse quis eleifend orci.', 1, 1);
+INSERT INTO sections (article_id, title, body, category_id, sequence) VALUES (2, 'Basics', 'Some basics about the importance of inheritance and stuff', 2, 1);
+INSERT INTO sections (article_id, title, body, category_id, sequence) VALUES (2, 'Examples', 'Some examples about squares being rectangles or something like that',2, 2);
+INSERT INTO sections (article_id, title, body, category_id, sequence) VALUES (2, 'More examples', 'Some more examples about fruits and vegetables',2, 3);
 INSERT INTO sections (article_id, title, body, category_id, sequence) VALUES (3, 'Intro to polymorph', 'Things can often times be more than one thing.', 3, 1);
+INSERT INTO sections (article_id, title, body, category_id, sequence) VALUES (1, 'Sketching', 'Doodle doodle doodle', 4, 1);
 
 --inserting section/versions
-INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) values (1, 1, 'Ones who know binnary and ones who do not', 3, 'Accepted');
-INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) values (2, 2, 'Some basics about the importance of inheritance and stuff', 2, 'Rejected');
-INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) values (3, 3, 'Some examples about squares being rectangles or something like that', 1, 'Accepted');
-INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) values (4, 1, 'Some examples about squares about fruits and vegetables', 2, 'Rejected');
+INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) VALUES (1, 1, 'Ones who know binnary and ones who do not', 3, 'Accepted');
+INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) VALUES (2, 2, 'Some basics about the importance of inheritance and stuff', 2, 'Rejected');
+INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) VALUES (3, 3, 'Some examples about squares being rectangles or something like that', 1, 'Accepted');
+INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) VALUES (4, 1, 'Some examples about squares about fruits and vegetables', 2, 'Rejected');
 INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) VALUES (5, 2, 'Things can often times be more than one thing.', 1, 'Accepted');
+INSERT INTO section_versions (section_id, owner_id, body, auditor_id, status) VALUES (6, 2, 'Doodle doodle doodle', 1, 'Accepted');
+
 
 --inserting resources
-INSERT INTO resources(section_id, title, owner_id, body) values (1, 'Who originally said this?', 3, 'Check this blog post about this silly joke.');
-INSERT INTO resources(section_id, title, owner_id, body) values (2, 'What is this', 2, 'who knows');
+INSERT INTO resources(section_id, title, owner_id, body) VALUES (1, 'Who originally said this?', 3, 'Check this blog post about this silly joke.');
+INSERT INTO resources(section_id, title, owner_id, body) VALUES (2, 'What is this', 2, 'who knows');
 INSERT INTO resources (section_id, title, owner_id, body) VALUES (3, 'second resource for this', 1, 'Hope this works!');
